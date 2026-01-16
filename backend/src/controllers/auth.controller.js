68a454f2d58d1c641ffa6e2d0c14c5c4e06c6bf8
Abhishek Yadav <165659574+Abhishekyaddv@users.noreply.github.com>
2026-01-16 17:53:21 +0530

    login refractoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -75,6 +75,10 @@ export const signup = async (req, res) => {
 export const login = async (req, res) =>{
   const {email, password} = req.body
 
+  if(!email || !password){
+    return res.status(400).json({message: "All fields are required"})
+  }
+
   try {
     // check for exisiting Email in database
     const user = await User.findOne({email})
